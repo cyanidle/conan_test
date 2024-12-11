233b825bbc2eb9596630de1ca83dec05b346f751
Alexej Doronin <lyosha.doronin@gmail.com>
2024-12-11 22:49:29 +0200

    Add files

---
+++ conan/conanfile.py
@@ -0,0 +1,46 @@
+import os, shutil
+from conan import ConanFile
+from conan.tools.files import save, load, copy
+from conan.tools.cmake import cmake_layout, CMake
+
+class Damn(ConanFile):
+    name = "damn"
+    version = "1.0"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def export_sources(self):
+        # The path of the CMakeLists.txt and sources we want to export are one level above
+        folder = os.path.join(self.recipe_folder, "..")
+        copy(self, "*.txt", folder, self.export_sources_folder)
+        copy(self, "src/*.cpp", folder, self.export_sources_folder)
+        copy(self, "include/*.hpp", folder, self.export_sources_folder)
+
+    def requirements(self):
+        self.requires("fmt/11.0.2")
+        self.requires("libdatachannel/0.22.2")
+
+    def source(self):
+        # Check that we can see that the CMakeLists.txt is inside the source folder
+        cmake_file = load(self, "CMakeLists.txt")
+
+    def layout(self):
+        # The root of the project is one level above
+        self.folders.root = ".." 
+        # The source of the project (the root CMakeLists.txt) is the source folder
+        self.folders.source = "."  
+        self.folders.build = "build"
+
+    
+    def build(self):
+        # Check that the build() method can also access the CMakeLists.txt in the source folder
+        path = os.path.join(self.source_folder, "CMakeLists.txt")
+        cmake_file = load(self, path)
+
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
---
+++ src/damn.cpp
@@ -0,0 +1,6 @@
+#include "damn.hpp"
+#include <fmt/core.h>
+
+std::string damn() {
+    return fmt::format("Hello {}!", "gamers");
+}
